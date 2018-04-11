89276ca9bc23861febff40d49b3996fd68db2062
=Canh Vu <=kanhknight@gmail.com>
2018-04-11 13:56:25 +0700

    Nested conditionals Example

---
+++ Session2/Homework/PartB/nested_conditional.py
@@ -0,0 +1,10 @@
+a = float(input("Nhập vào số A: "))
+b = float(input("Nhập vào số B: "))
+
+if a < b:
+    print("A nhỏ hơn B")
+else:
+    if a > b:
+        print("A lớn Hơn B")
+    else:
+        print("A và B Bằng Nhau")
