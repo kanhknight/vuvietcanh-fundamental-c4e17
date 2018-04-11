928d9350140eceaedcf3b36c6f2ce66752535de9
=Canh Vu <=kanhknight@gmail.com>
2018-04-11 12:39:57 +0700

    Ví dụ về các toán tử so sánh

--- Session2/Homework/PartB/boolean_example.py
+++ Session2/Homework/PartB/boolean_example.py
@@ -1,4 +1,10 @@
 # Boolean values are True and False
+# x == y -Produce True if ... x is equal to y
+# x != y -... x is not equal to y
+# x > y -... x is greater than y
+# x < y -.. x is less than y
+# x >= y -... x is greater than or equal to y
+# x <= y -.. x is less than or equal to y
 print("Ví dụ 1:")
 n = float(input("Nhập vào số thứ nhất: "))
 m = float(input("Nhập vào số thứ hai: "))
@@ -11,3 +17,21 @@ elif n > m:
 else:
     print("Số thứ nhất nhỏ hơn")
     print(bool(n<m))
+
+test = []
+print(test,'is',bool(test))
+
+test = [0]
+print(test,'is',bool(test))
+
+test = 0.0
+print(test,'is',bool(test))
+
+test = None
+print(test,'is',bool(test))
+
+test = True
+print(test,'is',bool(test))
+
+test = 'Easy string'
+print(test,'is',bool(test))
