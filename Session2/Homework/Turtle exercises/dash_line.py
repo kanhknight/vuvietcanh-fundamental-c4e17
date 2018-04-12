6d89d7f6500b1aeedf0033cd3d5f14a9adfcbc92
=Canh Vu <=kanhknight@gmail.com>
2018-04-12 12:27:11 +0700

    Dash line

---
+++ Session2/Homework/Turtle exercises/dash_line.py
@@ -0,0 +1,10 @@
+from turtle import *
+shape("turtle")
+pensize(3)
+color("red")
+for num in range(8):
+    penup()
+    forward(10)
+    pendown()
+    forward(10)  
+mainloop()
--- Session2/Homework/Turtle exercises/multilple_rhombus.py
+++ Session2/Homework/Turtle exercises/multilple_rhombus.py
@@ -10,5 +10,4 @@ for i in range(4):
         right(60)
         forward(50)
         right(60)
-
 mainloop()
