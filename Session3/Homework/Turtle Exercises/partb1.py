22d3b70ff0981475379896c1be1c79898b16dc56
=Canh Vu <=kanhknight@gmail.com>
2018-04-16 03:40:01 +0700

    PartB1

---
+++ Session3/Homework/Turtle Exercises/partb1.py
@@ -0,0 +1,24 @@
+from turtle import *
+colors = ['red','blue','brown','yellow','grey']
+pensize(2)
+for i in range(3):
+    color(colors[0])
+    forward(100)
+    left(360/3)
+for i in range(4):
+    color(colors[1])
+    forward(100)
+    left(360/4)
+for i in range(5):
+    color(colors[2])
+    forward(100)
+    left(360/5)
+for i in range(6):
+    color(colors[3])
+    forward(100)
+    left(360/6)
+for i in range(7):
+    color(colors[4])
+    forward(100)
+    left(360/7)
+mainloop()
