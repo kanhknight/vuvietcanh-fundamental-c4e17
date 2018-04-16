55679ed5f81945a7c76ab8dd91607f97b583812a
=Canh Vu <=kanhknight@gmail.com>
2018-04-16 23:07:48 +0700

    Fix bug Serius 2.6

--- Session3/Homework/Serious exercises/flockofsheep2_5.py
+++ Session3/Homework/Serious exercises/flockofsheep2_5.py
@@ -1,7 +1,13 @@
 # 2.5
 print("Hello, my name is Canh and these are my ship sizes")
 sheepsize = [5,7,300,90,24,50,75]
+print(sheepsize)
 n = int(input("Enter the number of months: "))
+maxsize = max(sheepsize)
+print("{0} {1} {2}".format("Now my biggest sheep has size", maxsize, "let's shear it"))
+a = sheepsize.index(maxsize)
+sheepsize[a] = 8
+print("After shearing, here is my flock"+"\n",sheepsize)
 for i in range(n):
     print("{0} {1}:".format("MONTH",i+1))
     for j in range(len(sheepsize)):
