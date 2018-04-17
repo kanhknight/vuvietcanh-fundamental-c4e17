fb4426c5e66329a2d8f3c46daf8b3faa9e7903c8
=Canh Vu <=kanhknight@gmail.com>
2018-04-17 20:05:33 +0700

    Save

--- Session3/Homework/Turtle Exercises/partb1.py
+++ Session3/Homework/Turtle Exercises/partb1.py
@@ -1,24 +1,21 @@
 from turtle import *
-colors = ['red','blue','brown','yellow','grey']
-pensize(2)
-for i in range(3):
-    color(colors[0])
-    forward(100)
-    left(360/3)
-for i in range(4):
-    color(colors[1])
-    forward(100)
-    left(360/4)
-for i in range(5):
-    color(colors[2])
-    forward(100)
-    left(360/5)
-for i in range(6):
-    color(colors[3])
-    forward(100)
-    left(360/6)
-for i in range(7):
-    color(colors[4])
-    forward(100)
-    left(360/7)
-mainloop()
+color_list = ['red','blue','brown','yellow','grey']
+n = int(input("Nhập vào 1 số: "))
+speed(0)
+if n>=3:
+    m = []
+    pensize(2)
+    for i in range(n):
+        m.append(i+1)
+    for j in m:
+        if j == 3:
+            for i in range(j):
+                forward(100)
+                left(360/j)
+        if j>3:
+            for i in range(j):
+                forward(100)
+                left(360/j)
+    mainloop()
+else:
+    print("Nhập vào 1 số lớn hơn hoặc bằng 3")
