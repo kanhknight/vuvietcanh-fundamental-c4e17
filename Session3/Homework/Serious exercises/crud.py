88cfa63de8bb65275ba1a20760ee1781e9d52f5c
=Canh Vu <=kanhknight@gmail.com>
2018-04-17 19:04:26 +0700

    Cập Nhật Bài CRUD

--- Session3/Homework/Serious exercises/crud.py
+++ Session3/Homework/Serious exercises/crud.py
@@ -1,25 +1,26 @@
 items = ["T-shirt", "Sweater", "Jeans"]
-n = input("Welcome to our shop, what do you want (C, R, U, D)? ")
-if n == "R":
-    print("Our items: ", end = "")
-    print(*items, sep = ", ")
-if n == "C":
-    item = input("Enter new item: ")
-    items.append(item)
-    print("Our items: ", end = "")
-    print(*items, sep = ", ")
-if n == "U":
-    position = int(input("Update Position: "))
-    position = position -1
-    update_item = input("New item? ")
-    items[position] = update_item
-    print("Our items: ", end = "")
-    print(*items, sep = ", ")
-if n == "D":
-    position = int(input("Delete Position: "))
-    position = position -1
-    # del items[position];
-    itemx = items[position]
-    items.remove(itemx)
-    print("Our items: ", end = "")
-    print(*items, sep = ", ")
+while True:
+    n = input("Welcome to our shop, what do you want (C, R, U, D)? ")
+    if n == "R":
+        print("Our items: ", end = "")
+        print(*items, sep = ", ")
+    elif n == "C":
+        item = input("Enter new item: ")
+        items.append(item)
+        print("Our items: ", end = "")
+        print(*items, sep = ", ")
+    elif n == "U":
+        position = int(input("Update Position: "))
+        position = position -1
+        update_item = input("New item? ")
+        items[position] = update_item
+        print("Our items: ", end = "")
+        print(*items, sep = ", ")
+    elif n == "D":
+        position = int(input("Delete Position: "))
+        position = position -1
+        # del items[position];
+        itemx = items[position]
+        items.remove(itemx)
+        print("Our items: ", end = "")
+        print(*items, sep = ", ")
