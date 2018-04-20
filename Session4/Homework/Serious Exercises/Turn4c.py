d85393719bf11060ed4ec699c6bd8e028e91248e
=Canh Vu <=kanhknight@gmail.com>
2018-04-20 14:27:26 +0700

    Trả bài turn 4c

--- Session4/Homework/Serious Exercises/Turn4c.py
+++ Session4/Homework/Serious Exercises/Turn4c.py
@@ -1,15 +1,19 @@
-n_list=[]
-n = int(input("Enter a number: "))
-for i in range(n):
-    n_list.append(i+1)
-
-for i in range(len(n_list)):
-    if i == 0:
-        print(*n_list)
-    else:
-        j_list = []
-        for j in n_list:
-            j = n_list[0] * (i+1)
-            j_list.append(j)
-        print(*j_list)
-    
+# for i in range(1,10):
+#     for j in range(1,10):
+#         print (i*j, end = " ")
+#     print()
+n = int(input("Enter a number: \n"))
+n_list = []
+for j in range(n):
+    n_list = []
+    for i in range(n):
+        n_list.append(i+1)
+    for k in range(len(n_list)):
+        n_list[k] = n_list[k]* (j+1)
+    for m in n_list:
+        if m < 10:
+            print(m,end = "   ")
+        elif m >=10:
+            print(m,end = "  ")
+    print()
+print()
