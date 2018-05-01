330657c643e34cb4c0f041ee2a701c03f9c341b1
=Canh Vu <=kanhknight@gmail.com>
2018-05-01 18:10:07 +0700

    Trả Bài 4 Phrase 2

--- Session6/Homework/Serious Exercise/count_customers.py
+++ Session6/Homework/Serious Exercise/count_customers.py
@@ -1,4 +1,8 @@
 from pymongo import MongoClient
+import matplotlib
+matplotlib.use("TkAgg")
+from matplotlib import pyplot
+
 uri = "mongodb://admin:admin@ds021182.mlab.com:21182/c4e"
 client = MongoClient(uri)
 db = client.get_default_database()
@@ -23,3 +27,18 @@ for kh in all_customer:
 for key, value in ref_kind.items():
     if value!=0:
         print("{0}: {1}".format(key, value))
+
+# Matpolib
+# 1 Chuẩn bị data
+loai_ref = [ref_kind["events"], ref_kind["ads"], ref_kind["wom"]]
+
+# 2. Chuẩn bị labels
+ten_ref = ["Events", "WOM", "ADS"]
+
+# Vẽ hình
+pyplot.title("Customers group by Refs")
+pyplot.pie(loai_ref, labels=ten_ref, autopct="%.1f%%", shadow=True, explode=[0,0.1,0.1])
+pyplot.axis("equal")
+
+# Show hàng
+pyplot.show()
