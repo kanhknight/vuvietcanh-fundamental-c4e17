9977cb70640d2d0f35838cdb07dc532fb050efd1
=Canh Vu <=kanhknight@gmail.com>
2018-05-03 21:32:29 +0700

    Bài 2 - Session 7

--- Session7/Homework/itune.py
+++ Session7/Homework/itune.py
@@ -1,4 +1,5 @@
 import pyexcel
+from youtube_dl import YoutubeDL
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
@@ -15,6 +16,14 @@ ul = div.find("ul")
 li_list  = ul.find_all("li")
 
 song_list = []
+option = {
+    'default_search': 'ytsearch',
+    'max_downloads':200,
+    'outtmpl': 'downloadedsongs/%(title)s.%(ext)s',
+    'format': 'best'
+}
+
+dl  =YoutubeDL(option)
 
 for li in li_list:
     a_song = li.h3.a
@@ -26,11 +35,12 @@ for li in li_list:
         "Artis":a_artis_name
     }
     song_list.append(item)
-print(song_list)
 
-pyexcel.save_as(records=song_list, dest_file_name = "itune.xlsx")
+# pyexcel.save_as(records=song_list, dest_file_name = "itune.xlsx")
 # itune_file = open("itune.html", "w")
 # # lưu thô raw_data ghi wb = "write binary"
 # itune_file.write(text_data)
 # itune_file.close()
 
+for song_item in song_list:
+    dl.download(song_item['Songs'])
