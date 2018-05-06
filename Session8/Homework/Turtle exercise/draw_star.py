20d676f671be9912264d02e58b242774f3287f24
=Canh Vu <=kanhknight@gmail.com>
2018-05-07 02:01:18 +0700

    Star - Session8

---
+++ Session8/Homework/Turtle exercise/draw_star.py
@@ -0,0 +1,17 @@
+import turtle 
+
+star = turtle.Turtle()
+
+# for i in range(50):
+#     star.forward(50)
+#     star.right(144)
+    
+# turtle.done()
+def draw_star(x,y,length):
+    star = turtle.Turtle()
+    star.goto(x,y)
+    for i in range(5):
+        star.forward(length)
+        star.right(144)
+draw_star(-50,50,300)
+turtle.mainloop()
