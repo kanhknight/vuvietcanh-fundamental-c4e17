e2be0ec60ae821d833d03252bc2bda0ad98f86a8
=Canh Vu <=kanhknight@gmail.com>
2018-05-08 01:21:06 +0700

    Sửa Bài 7 - Session 8

--- Session8/Homework/Serious Excercise/seven.py
+++ Session8/Homework/Serious Excercise/seven.py
@@ -1,7 +1,5 @@
 def remove_dollar_sign(s):
-    n = "This is Dollas strings: $ $ $ $ $ $"
-    print(n)
-    a= n.replace("$", "{0}".format(s))
+    a= s.replace("$", "")
     print(a)
 in_put = input("Nhập vào chuỗi muốn thay thế: ")
 remove_dollar_sign(in_put)
