7547542a16231e05853ac6cddf27e78e1c2df94d
=Canh Vu <=kanhknight@gmail.com>
2018-05-08 11:10:44 +0700

    Fix bug bài rand ngôi sao

--- Session8/Homework/Turtle exercise/rand_int.py
+++ Session8/Homework/Turtle exercise/rand_int.py
@@ -5,8 +5,10 @@ color('blue')
 def draw_star(x,y,length):
     goto(x,y)
     for i in range(5):
+        pendown()
         forward(length)
         right(144)
+        penup()
 
 for i in range(100):
     import random
