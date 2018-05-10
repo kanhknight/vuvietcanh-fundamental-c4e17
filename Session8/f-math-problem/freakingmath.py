3654004c1cb4e321ce842bdd7cfec147f996c4b0
=Canh Vu <=kanhknight@gmail.com>
2018-05-10 22:06:36 +0700

    Trả bài Freaking Marth Tuấn Anh Giao

--- Session8/f-math-problem/freakingmath.py
+++ Session8/f-math-problem/freakingmath.py
@@ -2,8 +2,38 @@ from random import *
 
 def generate_quiz():
     # Hint: Return [x, y, op, result]
-    return [0, 0, '+', 10]
+    x = randint(1,10)
+    y = randint(1,10)
+    op = ["*","/","+","-"]
+    result_right = 0
+    op_rand = choice(op)
+
+    if op_rand == "*":
+        result_right = x * y
+    elif op_rand == "/":
+        result_right = x/y
+    elif op_rand == "+":
+        result_right = x+y
+    elif op_rand == "-":
+        result_right == x - y
+    err = randint(-1,1)
+    result = result_right + err
+    return [x, y, op_rand, result]
 
 def check_answer(x, y, op, result, user_choice):
-    return True
     # User Choice : True/False
+    real_result = 0
+    if op == "*":
+        real_result = x * y
+    elif op == "/":
+        real_result = x/y
+    elif op == "+":
+        real_result = x + y
+    elif op == "-":
+        real_result = x -y
+    if real_result == result and user_choice == True:
+        return True
+    elif real_result !=result and user_choice == False:
+        return True
+    else:
+        return False
