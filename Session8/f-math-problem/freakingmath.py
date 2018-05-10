5cfac5391008ddc749c35b83eb42899999a7a8d4
=Canh Vu <=kanhknight@gmail.com>
2018-05-10 23:42:36 +0700

    Cập Nhật hàm calc Freaking Math

--- Session8/f-math-problem/freakingmath.py
+++ Session8/f-math-problem/freakingmath.py
@@ -1,39 +1,35 @@
 from random import *
 
+def calc(x,y,op):
+    result = 0
+    if op == "*":
+        result = x * y
+    elif op == "/":
+        result = x/y
+    elif op == "+":
+        result = x + y
+    elif op == "-":
+        result = x -y
+    return [x,y,op,result]
+
 def generate_quiz():
     # Hint: Return [x, y, op, result]
     x = randint(1,10)
     y = randint(1,10)
-    op = ["*","/","+","-"]
-    result_right = 0
-    op_rand = choice(op)
-
-    if op_rand == "*":
-        result_right = x * y
-    elif op_rand == "/":
-        result_right = x/y
-    elif op_rand == "+":
-        result_right = x+y
-    elif op_rand == "-":
-        result_right == x - y
+    op_list = ["*","/","+","-"]
+    op = choice(op_list)
     err = randint(-1,1)
-    result = result_right + err
-    return [x, y, op_rand, result]
+    result = calc(x,y,op)
+    result = result[3]
+    result = result + err
+    return [x, y, op, result]
 
 def check_answer(x, y, op, result, user_choice):
     # User Choice : True/False
-    real_result = 0
-    if op == "*":
-        real_result = x * y
-    elif op == "/":
-        real_result = x/y
-    elif op == "+":
-        real_result = x + y
-    elif op == "-":
-        real_result = x -y
-    if real_result == result and user_choice == True:
+    real_result = calc(x,y,op)
+    if real_result[3] == result and user_choice == True:
         return True
-    elif real_result !=result and user_choice == False:
+    elif real_result[3] != result and user_choice == False:
         return True
     else:
         return False
